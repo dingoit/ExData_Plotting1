fd40b017b6de5af84f43678c8689a78deac34285
dingoit <mtecilla@gmail.com>
2014-05-07 20:49:54 +0200

    Assignment 1, Exploratory Data Analysis, Coursera

--- plot1.R
+++ plot1.R
@@ -2,7 +2,7 @@
 ## Course Project 1
 ## Plot 1
 
-hpc <- read.table("household_power_consumption.txt", skip = 66637, nrow = 2880, sep = ";", 
+hpc <- read.table("household_power_consumption.txt", skip = 66637, nrow = 2880, sep = ";", na.strings="?", 
                   col.names = colnames(read.table("household_power_consumption.txt",nrow = 1, header = TRUE, sep=";")))
 
 png(file = "plot1.png", bg = "transparent", width = 480, height = 480, units = "px")
--- plot4.R
+++ plot4.R
@@ -2,7 +2,7 @@
 ## Course Project 1
 ## Plot 4
 
-hpc <- read.table("household_power_consumption.txt", skip = 66637, nrow = 2880, sep = ";", 
+hpc <- read.table("household_power_consumption.txt", skip = 66637, nrow = 2880, sep = ";", na.strings="?", 
                   col.names = colnames(read.table("household_power_consumption.txt",nrow = 1, header = TRUE, sep=";")))
 hpc$DT <- strptime(paste(hpc$Date, hpc$Time), format="%d/%m/%Y %H:%M:%S")
 
